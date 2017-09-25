155588b38c486a84fa304639455370e3fcef6d99
muriithiKabogo <dmuriithi.k@gmail.com>
2017-09-25 15:14:57 +0000

    heroku first comit

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,8 +7,39 @@ class DashboardController < ApplicationController
 
   end
 
-  def list
+  def churn
   	
+  end
+
+  def most_valuable
+
+  end
+
+  def likely_convert
+
+  end
+
+  def churn_onboarding
+  	
+  end
+
+  def campaign
+
+  end
+
+  def explore
+    
+  end
+
+  def segment
+    
+  end
+
+  def create_segment
+    
+  end
+
+  def compose_segmented
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   get 'dashboard/new'
   get 'dashboard/one'
-  get 'dashboard/list'
+  get 'dashboard/churn'
+  get 'dashboard/most_valuable'
+  get 'dashboard/likely_convert'
+  get 'dashboard/churn_onboarding'
+  get 'dashboard/campaign'
+  get 'dashboard/compose'
+  get 'dashboard/segment'
+  get 'dashboard/create_segment'
+  get 'dashboard/compose_segmented'
   root 'dashboard#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
