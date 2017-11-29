b0d3e980427f80de8c7341f48375b28fde7c260e
muriithiKabogo <dmuriithi.k@gmail.com>
2017-11-29 11:11:19 +0300

    adds a login,sign in and sign out

--- db/schema.rb
+++ db/schema.rb
@@ -15,63 +15,6 @@ ActiveRecord::Schema.define(version: 20171128201018) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "api_key", id: :serial, force: :cascade do |t|
-    t.string "project", limit: 255, null: false
-    t.string "read_key", limit: 255, null: false
-    t.string "write_key", limit: 255, null: false
-    t.string "master_key", limit: 255, null: false
-    t.datetime "created_at", default: -> { "now()" }, null: false
-  end
-
-  create_table "config", primary_key: ["project", "name"], force: :cascade do |t|
-    t.string "project", limit: 255, null: false
-    t.string "name", limit: 255, null: false
-    t.text "value"
-  end
-
-  create_table "continuous_query_metadata", primary_key: ["project", "table_name"], force: :cascade do |t|
-    t.string "project", limit: 255, null: false
-    t.string "name", limit: 255, null: false
-    t.string "table_name", limit: 255, null: false
-    t.text "query", null: false
-    t.text "partition_keys"
-    t.text "options"
-  end
-
-  create_table "custom_data_source", primary_key: ["project", "schema_name"], force: :cascade do |t|
-    t.string "project", limit: 255, null: false
-    t.string "schema_name", limit: 255, null: false
-    t.text "type", null: false
-    t.text "options"
-  end
-
-  create_table "custom_file_source", primary_key: ["project", "table_name"], force: :cascade do |t|
-    t.string "project", limit: 255, null: false
-    t.string "table_name", limit: 255, null: false
-    t.text "options"
-  end
-
-  create_table "javascript_logs", id: false, force: :cascade do |t|
-    t.text "id", null: false
-    t.string "project", limit: 255, null: false
-    t.string "type", limit: 15, null: false
-    t.string "prefix", limit: 255, null: false
-    t.text "error", null: false
-    t.bigint "created", default: 0, null: false
-  end
-
-  create_table "materialized_views", primary_key: ["project", "table_name"], force: :cascade do |t|
-    t.string "project", limit: 255, null: false
-    t.string "name", limit: 255, null: false
-    t.string "table_name", limit: 255, null: false
-    t.text "query", null: false
-    t.bigint "update_interval"
-    t.bigint "last_updated"
-    t.boolean "incremental"
-    t.text "options"
-    t.boolean "real_time", default: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
