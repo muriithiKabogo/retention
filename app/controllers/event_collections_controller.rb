46e6a4aaf5e96d5700c2b046260fd66ea8472e9c
muriithiKabogo <dmuriithi.k@gmail.com>
2017-12-08 09:41:48 +0300

    gets readKey from DB

--- app/controllers/event_collections_controller.rb
+++ app/controllers/event_collections_controller.rb
@@ -7,19 +7,19 @@ class EventCollectionsController < ApplicationController
   def index
   	string = collection.read_body
   	parsed = JSON.parse(string)
-	@events = parsed["result"]
+	  @events = parsed["result"]
   end
 
   private
 
   def collection	
-  	url = URI("https://nestmetricai.herokuapp.com//event-explorer/statistics")
-  	http = Net::HTTP.new(url.host, url.port)
+  url = URI("https://nestmetricai.herokuapp.com//event-explorer/statistics")
+  http = Net::HTTP.new(url.host, url.port)
 	http.use_ssl = true
 	http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
 	request = Net::HTTP::Post.new(url)
-	request["read_key"] = ENV["rakam_read_key"]
+	request["read_key"] = current_user.projects.first.readKey
 	request.body = "{\"measure\":{},\"startDate\":\"2017-11-20\",\"endDate\":\"2017-12-03\",\"collections\":[\"pageview\"]}"
 
 	response = http.request(request)
