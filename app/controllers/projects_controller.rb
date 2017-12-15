5b329a1e0cc2de01aea14babd816616ad7c297ba
muriithiKabogo <dmuriithi.k@gmail.com>
2017-12-15 12:36:31 +0300

    adds an alert message on successful project creation

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,26 +4,33 @@ require 'net/http'
 class ProjectsController < ApplicationController
 before_action :authenticate_user!
   def new
-  	@project = Project.new
+    @user = current_user
+    if @user.projects.exists?
+      redirect_to  dashboard_most_valuable_url
+    else
+  	   @project = Project.new
+    end
   end
 
   def create
-  	@user = current_user
-  	project = project_params[:projectName]
-  	website = project_params[:website]
+    
+    	@user = current_user
+    	project = project_params[:projectName]
+    	website = project_params[:website]
 
-  	trimmed_response = createProject(project).read_body.tr('{}','')
-  	array_response = trimmed_response.split(',')
-  	master_key_array = array_response[0].split(':')
-  	read_key_array = array_response[1].split(':')
-  	write_key_array = array_response[2].split(':')
-    
-  	master_key = master_key_array[1]
-  	read_key = read_key_array[1]
-  	write_key = write_key_array[1]
-    
-  	@user.projects.create(projectName: project,masterKey: master_key,readKey: read_key,writeKey: write_key,website: website)
-  	redirect_to dashboard_one_url
+    	trimmed_response = createProject(project).read_body.tr('{}','')
+    	array_response = trimmed_response.split(',')
+    	master_key_array = array_response[0].split(':')
+    	read_key_array = array_response[1].split(':')
+    	write_key_array = array_response[2].split(':')
+      
+    	master_key = master_key_array[1]
+    	read_key = read_key_array[1]
+    	write_key = write_key_array[1]
+      
+    	@user.projects.create(projectName: project,masterKey: master_key,readKey: read_key,writeKey: write_key,website: website)
+      flash[:success] = "Project created"
+    	redirect_to dashboard_one_url
   end
 
   def edit
