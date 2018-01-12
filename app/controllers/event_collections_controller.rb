20b0db1b893b507e56676cff5b994e85936cb197
muriithiKabogo <dmuriithi.k@gmail.com>
2018-01-12 08:15:28 +0300

    output variable in event collection controller

--- app/controllers/event_collections_controller.rb
+++ app/controllers/event_collections_controller.rb
@@ -25,7 +25,7 @@ class EventCollectionsController < ApplicationController
 	http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
 	request = Net::HTTP::Post.new(url)
-	request["read_key"] = current_user.projects.first.readKey
+	request["read_key"] = "92prrm43kth6avmd5e8aiq1avi2h1ucg2nig4ja7ondhabmpufmft7gqagov1g9i"
 	request.body = "{\"measure\":{},\"startDate\":\"2017-11-20\",\"endDate\":\"#{Date.today.to_s}\",\"collections\":[\"pageview\"]}"
 
 	response = http.request(request)
