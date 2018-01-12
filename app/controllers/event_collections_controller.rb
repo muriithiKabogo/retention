47ba61e5e0197498ca8f23c87614cf195e3824d6
muriithiKabogo <dmuriithi.k@gmail.com>
2018-01-12 08:49:57 +0300

    output

--- app/controllers/event_collections_controller.rb
+++ app/controllers/event_collections_controller.rb
@@ -21,7 +21,7 @@ class EventCollectionsController < ApplicationController
 
 	request = Net::HTTP::Post.new(url)
   puts user.projects.first.readKey
-	request["read_key"] = current_user.projects.first.readKey
+	request["read_key"] = user.projects.first.readKey
 	request.body = "{\"measure\":{},\"startDate\":\"2017-11-20\",\"endDate\":\"#{Date.today.to_s}\",\"collections\":[\"pageview\"]}"
 
 	response = http.request(request)
