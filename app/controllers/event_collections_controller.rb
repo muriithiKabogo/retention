ec3699bc618ce0da4672230d7ae341c3620af53d
muriithiKabogo <dmuriithi.k@gmail.com>
2018-01-12 08:45:54 +0300

    output

--- app/controllers/event_collections_controller.rb
+++ app/controllers/event_collections_controller.rb
@@ -6,21 +6,21 @@ class EventCollectionsController < ApplicationController
   
   def index
     @user = current_user
-  	string = collection.read_body
+  	string = collection(@user).read_body
   	parsed = JSON.parse(string)
 	  @events = parsed["result"]
   end
 
   private
 
-  def collection	
+  def collection(user)	
   url = URI("https://nestmetricai.herokuapp.com///event-explorer/statistics")
   http = Net::HTTP.new(url.host, url.port)
 	http.use_ssl = true
 	http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
 	request = Net::HTTP::Post.new(url)
-  puts current_user.projects.first.readKey
+  puts user.projects.first.readKey
 	request["read_key"] = current_user.projects.first.readKey
 	request.body = "{\"measure\":{},\"startDate\":\"2017-11-20\",\"endDate\":\"#{Date.today.to_s}\",\"collections\":[\"pageview\"]}"
 
