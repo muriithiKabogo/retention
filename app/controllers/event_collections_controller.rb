1534ae15fda45d9e9cc6d64dd6572f646342ecdd
muriithiKabogo <dmuriithi.k@gmail.com>
2018-01-15 05:29:05 +0300

    puts the key in the costroller

--- app/controllers/event_collections_controller.rb
+++ app/controllers/event_collections_controller.rb
@@ -20,6 +20,7 @@ class EventCollectionsController < ApplicationController
   	string = response.read_body
   	parsed = JSON.parse(string)
 	  @events = parsed["result"]
+    puts @events
   end
 
   private
