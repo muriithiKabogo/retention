99a1063e2715efa1c9a3be847b0044d69d0aab2d
muriithiKabogo <dmuriithi.k@gmail.com>
2018-01-18 14:43:35 +0300

    view of all events DRY

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -14,7 +14,7 @@ class DashboardController < ApplicationController
     ActiveRecord::Base.connection.schema_search_path = "#{@project_name},public"
     puts ActiveRecord::Base.connection.schema_search_path
     @result = ActiveRecord::Base.connection.execute("select * from #{field}")
-    redirect_to dashboard_most_valuable
+    redirect_to dashboard_most_valuable_path
     #this code needs to be re-written
   end
 
