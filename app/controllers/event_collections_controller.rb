ec0525372d1bae27b4fd0f2a5ebb08aec1fa2b04
muriithiKabogo <dmuriithi.k@gmail.com>
2018-01-18 10:43:53 +0300

    uses a variable to output the  readKey

--- app/controllers/event_collections_controller.rb
+++ app/controllers/event_collections_controller.rb
@@ -13,7 +13,7 @@ class EventCollectionsController < ApplicationController
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
     request = Net::HTTP::Post.new(url) 
-    request["read_key"] = "#{@user.projects.first.readKey}" #{}"92prrm43kth6avmd5e8aiq1avi2h1ucg2nig4ja7ondhabmpufmft7gqagov1g9i"
+    request["read_key"] = @user.projects.first.readKey.to_s #{}"92prrm43kth6avmd5e8aiq1avi2h1ucg2nig4ja7ondhabmpufmft7gqagov1g9i"
     request.body = "{\"measure\":{},\"startDate\":\"2017-11-20\",\"endDate\":\"#{Date.today.to_s}\"}"
     response = http.request(request)
 
