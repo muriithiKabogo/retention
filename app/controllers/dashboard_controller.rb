c7e0fe43a902c3f8a83657403d771f8aa95c6c41
muriithiKabogo <dmuriithi.k@gmail.com>
2018-01-23 05:49:04 +0300

    changes the heading of dashboard/mostvaluable to be a variable

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -23,7 +23,7 @@ class DashboardController < ApplicationController
     @user = current_user
 
     @table = params[:projectName]
-    puts @table.class
+
     if @table.nil?
       @table = cookies[:table]
     end
@@ -35,6 +35,7 @@ class DashboardController < ApplicationController
     ActiveRecord::Base.connection.schema_search_path = "#{@project_name},public"
     puts ActiveRecord::Base.connection.schema_search_path
     @result = ActiveRecord::Base.connection.execute("select * from \"#{@table}\"")
+    puts @result.class
     #this code needs to be re-written
   end
   def likely_convert
