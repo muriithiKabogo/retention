9eb04961166b81417d10fecb835a6113fb324e25
muriithiKabogo <dmuriithi.k@gmail.com>
2018-01-23 13:11:46 +0300

    adds flash messages from devise

--- app/controllers/event_collections_controller.rb
+++ app/controllers/event_collections_controller.rb
@@ -13,7 +13,8 @@ class EventCollectionsController < ApplicationController
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
     request = Net::HTTP::Post.new(url) 
-    request["read_key"] = "92prrm43kth6avmd5e8aiq1avi2h1ucg2nig4ja7ondhabmpufmft7gqagov1g9i" # @user.projects.first.readKey 
+    request["read_key"] =  @user.projects.first.readKey
+    puts request["read_key"]
     request.body = "{\"measure\":{},\"startDate\":\"2017-11-20\",\"endDate\":\"#{Date.today.to_s}\"}"
     response = http.request(request)
 
