9a4959cc3f1792c023a94c0c3933b2f0c314b981
muriithiKabogo <dmuriithi.k@gmail.com>
2018-01-24 11:26:20 +0300

    debugging the events index controller

--- app/controllers/event_collections_controller.rb
+++ app/controllers/event_collections_controller.rb
@@ -11,9 +11,10 @@ class EventCollectionsController < ApplicationController
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
+    puts "The length of the key is #{@user.projects.first.readKey.length}"
     request = Net::HTTP::Post.new(url) 
-    request["read_key"] =  "92prrm43kth6avmd5e8aiq1avi2h1ucg2nig4ja7ondhabmpufmft7gqagov1g9i"#@user.projects.first.readKey
+    request["read_key"] = "92prrm43kth6avmd5e8aiq1avi2h1ucg2nig4ja7ondhabmpufmft7gqagov1g9i"#@user.projects.first.readKey
+    puts "The length of the hard coded readkey is #{request["read_key"].length}"
     request.body = "{\"measure\":{},\"startDate\":\"2017-11-20\",\"endDate\":\"#{Date.today.to_s}\"}"
     response = http.request(request)
 
