e747de8a216b68b3cf23c26d39ec03b7ce99c77a
muriithiKabogo <dmuriithi.k@gmail.com>
2018-03-27 22:15:53 +0300

    puts function to highlight a list of all users

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,5 +2,13 @@ class CustomersController < ApplicationController
   before_action :authenticate_user!
   def index
   	@user = current_user
+  	@project = @user.projects.first
+    @project_name = @project.projectName
+    ActiveRecord::Base.connection.schema_search_path = "#{@project_name},public"
+    puts ActiveRecord::Base.connection.schema_search_path
+    @customers = ActiveRecord::Base.connection.execute("select * from \"_users\"")
+
+    puts @customers.class
+    puts @customers
   end
 end
