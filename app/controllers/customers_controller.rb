53cea6c6ca794726f15dcedeaa9c81bdd0fb0f99
muriithiKabogo <dmuriithi.k@gmail.com>
2018-03-27 22:42:40 +0300

    outputs users on customers page

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -7,13 +7,5 @@ class CustomersController < ApplicationController
     ActiveRecord::Base.connection.schema_search_path = "#{@project_name},public"
     puts ActiveRecord::Base.connection.schema_search_path
     @customers = ActiveRecord::Base.connection.execute("select * from \"_users\"")
-
-    puts @customers.class
-    puts @customers.count
-    puts @customers
-
-    @customers.each do |customer|
-    	puts customer
-    end
   end
 end
